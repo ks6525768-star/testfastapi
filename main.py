13bb6d3250853f69f8248c80946b6e003f887c7b
ks6525768-star <ks6525768@gmail.com>
2025-11-20 11:00:28 -0300

    Set Python version to 3.11

--- main.py
+++ main.py
@@ -1,37 +1,35 @@
-import os
 from fastapi import FastAPI, Request
 from telegram import Update
-from telegram.ext import Application, CommandHandler
+from telegram.ext import Application
+import os
 
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-print("TOKEN LOADED:", BOT_TOKEN)
+TOKEN = os.getenv("BOT_TOKEN")
 
 app = FastAPI()
-bot_app = Application.builder().token(BOT_TOKEN).build()
 
+bot_app = Application.builder().token(TOKEN).build()
 
-#Telegram Commands
+# Initialize Telegram bot on startup
+@app.on_event("startup")
+async def startup_event():
+    await bot_app.initialize()
+    await bot_app.start()
+    print("Bot initialized and started")
 
-
-async def start(update: Update, context):
-    await update.message.reply_text("Hello! Bot is working with FastAPI + WEbhooks")
-
-
-bot_app.add_handler(CommandHandler("start", start))
-
-
-# Webhook Endpoint
+# Shutdown cleanly
+@app.on_event("shutdown")
+async def shutdown_event():
+    await bot_app.stop()
+    await bot_app.shutdown()
+    print("Bot stopped")
 
 @app.post("/webhook")
 async def telegram_webhook(request: Request):
     data = await request.json()
     update = Update.de_json(data, bot_app.bot)
     await bot_app.process_update(update)
-    return {"status: ok"}
+    return {"status": "ok"}
 
-
-#Root Check
 @app.get("/")
-def home():
-    return {"message": "Telegram bot running on FastAPI!"}
+async def root():
+    return {"status": "running"}
